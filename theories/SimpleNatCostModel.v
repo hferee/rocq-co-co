25d2f55671cd3be5ebb68efd950c05c59436b1b3
Hugo Férée <feree@irif.fr>
2026-05-20 11:54:54 +0200

    clean nat complexity

--- theories/SimpleNatCostModel.v
+++ theories/SimpleNatCostModel.v
@@ -35,21 +35,40 @@ Module Type NatTimeCostModel
   Definition ℂI_nat_inj (n : nat) := n ⋊ tt.
   Coercion ℂI_nat_inj : nat >-> ℂI.
 
+ (* TODO: are there (structural) fixpoints on nat that are not expressible this way?
+  And can we write a tactic to turn most such functions into a nat_rect?
+  And can we automatically prove they are extensionally equal? *)
+
+  (* Simple recursion with no additional argument *)
+  Definition nat_fix0 (B : Type) (v : B) (F : nat -> B -> B) :=
+    fix f (n : nat) : B := match n with O => v | S k => F k (f k) end.
+
+  Parameter nat_fix0_complexity : forall {B CB : Type}
+  (v : B) (F : nat -> B -> B) (cv : CB) cF,
+  let f := nat_fix0 B v F in
+    ComplexityBound v cv ->
+    ComplexityBound F (cF : ℂI nat unit -> ℂO (ℂI B CB -> ℂO CB)) ->
+    ComplexityBound (f : nat -> B)
+      (fun (cn : ℂI nat unit) =>
+      ((fix cfix n : ℂO (ℂI B CB) := 1 ⊕
+        match n with
+      | O => ret (v ⋊ cv)
+      | S k => (* the recursive call *)
+              cfix k O>>=O (k ⋊ tt I>>=I (F ⋊ cF))
+      end) (ival cn) >>|) : ℂO CB).
+
   (* Induction principle on nat, first for unary functions *)
-  (* TODO: this should be derivable from nat_fix1_complexity below *)
-  Parameter nat_complexity_rect1: forall {A CA} (v : A) (f : nat -> A -> A)
+  (* TODO: this should be derivable from nat_fix0_complexity *)
+  Lemma nat_complexity_rect1: forall {A CA} (v : A) (f : nat -> A -> A)
     (cv : CA) (cf: ℂI nat unit -> ℂO (ℂI A CA -> ℂO CA)),
     ComplexityBound v cv ->
     ComplexityBound f cf ->
     let g := nat_rect _ v f in
     ComplexityBound g
-      (fun (cn : ℂI nat unit) => (nat_rect (fun n => ℂO (ℂI A CA)) (ret (v ⋊ cv))
+      (fun (cn : ℂI nat unit) => (nat_rect (fun n => ℂO (ℂI A CA)) (1 ⊕ ret (v ⋊ cv))
                   (fun k gk =>
                     gk O>>=O ((k ⋊ tt I>>=I (f ⋊ cf)))) (ival cn)) >>|).
-
- (* TODO: are there (structural) fixpoints on nat that are not expressible this way?
-  And can we write a tactic to turn most such functions into a nat_rect?
-  And can we automatically prove they are extensionally equal? *)
+  Proof. Abort.
 
   (* A quite general complexity bound for fixpoints over nat with 1 additional
     argument. Hopefully I got it right. *)
@@ -119,34 +138,7 @@ Module NatTimeExamples (Import CM : CostModel)
   Unshelve. apply bound_order_ext_eq. intros. unfold OIbindO. reflexivity.
   Qed.
 
-   Example plus_complexity:
-    ComplexityBound plus (fun (n : ℂI nat unit) => 2 * ival n ⋉
-                          fun (m : ℂI nat unit) => (1 + 2 * ival n) ⋉ tt).
-  (* TODO: Weirdly, n appears in both bounds (should only appear on the first? *)
-  Proof.
-  change_fun_with (nat_rect _ id (fun k => compose S)).
-  capply.
-  (* TODO: a tactic should handle this *)
-  apply nat_complexity_rect1.
-  - eapply id_complexity.
-  - eapply constant_complexity.
-    eapply apply_complexity.
-    + apply S_complexity.
-    + apply comp1_complexity.
-  Unshelve.
-  * apply ext_eq_dep_fun. intro n. apply ext_eq_dep_fun.
-    intros m. apply ext_eq_eq. revert m; induction n; 
-    unfold compose; simpl; auto with *.
-  * (* TODO: automate this *)
-    apply bound_order_ext_eq. intros. apply bound_order_output; split.
-    -- simpl. induction (ival x); simpl; lia.
-    -- simpl. apply bound_order_ext_eq. intro. 
-       apply bound_order_output. split.
-       ++ simpl. induction (ival x); simpl; lia.
-       ++ apply bound_order_unit.
-  Qed.
-
-  Example plus_complexity':
+  Example plus_complexity:
     ComplexityBound plus (fun (n : ℂI nat unit) => 1 ⋉
                           fun (m : ℂI nat unit) => (6 * ival n + 2) ⋉ tt).
   Proof.
