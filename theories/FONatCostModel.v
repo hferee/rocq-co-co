a3c483891f30429501303731d7ddc196896ac177
Hugo Férée <feree@irif.fr>
2026-05-28 16:08:29 +0200

    Experiments with functions from the standard library over nat

--- theories/FONatCostModel.v
+++ theories/FONatCostModel.v
@@ -38,6 +38,23 @@ Module Type NatTimeCostModel
                 | S k => cf k
                 end).
   Existing Instance nat_match_complexity.
+  (* A slightly stronger version, where the matched integer is still bound in 
+    the subcases. Required for pred. *)
+  Parameter nat_match_complexity': forall {A} (v : nat -> A) (f : nat -> nat -> A)
+    (cv : nat -> nat) cf,
+    ComplexityBound v cv ->
+    ComplexityBound f cf ->
+    ComplexityBound 
+      (fun n => match n with
+                | O => v n
+                | S k => f n k
+                end)
+      (fun (n : nat) => 1 + (* 1 for the match *)
+                match n with
+                | O => cv n
+                | S k => cf n k
+                end).
+  Existing Instance nat_match_complexity'.
 
  (* TODO: are there (structural) fixpoints on nat that are not expressible this way?
   And can we write a tactic to turn most such functions into a nat_rect?
@@ -105,10 +122,23 @@ Parameter fo_compose_complexity : forall {A B C}
                   (fun a => cg a + cf (g a)) (* Looks like a derivation *).
   Global Existing Instance fo_compose_complexity.
 
+  (* Variant with 2 arguments. TODO: generalize *)
+  Parameter fo_compose_complexity2 : forall {A B B' C}
+  (f : B -> B' -> C) (cf : B -> B' -> nat) (g : A -> B) (g' : A -> B') (cg : A -> nat)
+  (cg' : A -> nat),
+  ComplexityBound f cf ->
+  ComplexityBound g cg ->
+  ComplexityBound g' cg' ->
+  ComplexityBound (fun n => f (g n) (g' n) : C)
+                  (fun a => cg a + cg' a + cf (g a) (g' a)).
+  Global Existing Instance fo_compose_complexity2.
+
   Parameter constant_complexity : forall {A B CB} b {cb},
   ComplexityBound (b : B) (cb : CB) ->
   ComplexityBound (fun (_ : A) => b) (fun (_ : A) => cb).
   Global Existing Instance constant_complexity.
+  
+  (* TODO: we need all of Corelib.Program.Basics. flip ; apply *)
 
   Parameter id_complexity : ComplexityBound (fun (x : nat) => x) (fun (_ : nat) => 1).
   Global Existing Instance id_complexity.
@@ -162,4 +192,120 @@ Ltac btac cf:=
     * subst; lia. (* trivial once you have the right bound *)
   Qed.
   Existing Instance mult_complexity.
+
+  Example pred_complexity:
+    ComplexityBound Nat.pred (fun (n : nat) => 2).
+  Proof.
+  unfold pred. capply.
+  (* Difficulty: for some reason, pred is n, not O, when n matches O *)
+  ctac.
+  Unshelve. simpl. btac cpred. destruct x; subst; lia.
+  Qed.
+
+  Example double_complexity:
+    ComplexityBound Nat.double (fun n => n * 3 + 4).
+  Proof.
+  unfold Nat.double. capply. ctac.
+  Unshelve. simpl. btac cpred. subst. lia.
+  Qed.
+
+Definition mysub :=
+(fix sub (n m : nat) {struct n} : nat :=
+     match n with
+     | 0 => 0
+     | S k => match m with
+              | 0 => (S k)
+              | S l => sub k l
+              end
+     end).
+
+  Example sub_complexity:
+    ComplexityBound mysub (fun n => n * 3 + 4).
+  Proof.
+  unfold mysub. capply.
+  (* Difficulties: as for pred, sub still binds n after matching over it.
+    Furthermore, it doesn't always make a recursive call when n = S k *)
+  Abort.
+  
+  Example eqb_complexity:
+    ComplexityBound Nat.eqb (fun n m => n + m).
+  Proof.
+  unfold Nat.eqb. capply.
+  (* Similar to sub. leb ltb, max, min compare will be similar *)
+  Abort.
+
+  Example even_complexity:
+    ComplexityBound Nat.even (fun n => S n).
+  Proof. unfold Nat.even. capply. Abort.
+  (* DIfficulty : match is not standard (base case 1).
+    Recursive call is made on "n - 2". *)
+
+  Example pow_complexity:
+    ComplexityBound Nat.pow (fun n m => n * m + 1).
+  Proof.
+  unfold Nat.pow. capply. Abort.
+  (* Difficulty: recursion is made on the second argument.
+    Similar to Nat.div. *)
+
+  (* Tail add has the same time complexity as add. Space complexity should be
+    different though. *)
+  Example tail_add_complexity:
+    ComplexityBound Nat.tail_add (fun n (m : nat) => 3 * n + 2).
+  Proof.
+  unfold Nat.tail_add. capply.
+  (* TODO: ctac fails here *)
+  apply (nat_fix1_complexity
+    (@id nat)
+    (fun (k x : nat) => S x)
+    (fun (k x : nat) r => r)).
+  - apply id_complexity. (* TODO: this should be automated by ctac *)
+  - ctac.
+  - ctac.
+  Unshelve. simpl. btac ctail.
+  + revert x0. induction x as [|n]; intro x0.
+  * simpl. subst; lia.
+  * simpl. specialize (IHn (S x0)). subst. lia.
+  Qed.
+  
+  Example square_complexity:
+    ComplexityBound Nat.square (fun n => 4 + n * (4 + 3 * n)).
+  Proof. unfold Nat.square. capply. ctac.
+  Unshelve.
+  simpl. apply bound_order_ext_eq; intro n. apply bound_order_nat.
+  remember ((fix cf (n0 a : nat) {struct n0} : nat :=
+         S
+           match n0 with
+           | 0 => 1
+           | S k => S (cf k a + (a + (a + (a + 0)) + 2))
+           end)) as cf.
+  do 2 apply le_n_S.
+  (* Difficulty: a bit of generalisation is necessary. *)
+  enough (Hnm: forall n m, cf n m <= n * (3 * m + 4) + 2).
+  { specialize (Hnm n n). lia. }
+  clear n. intros n m. induction n as [|n]; subst; lia.
+  Qed.
+  
+  (* TODO: Nat.sqrt_iter is a fixpoint with 4 arguments *)
+(* Nat.sqrt_iter
+     Definition divmod : nat -> nat -> nat -> nat -> nat * nat.
+     Definition div : nat -> nat -> nat.
+     Definition modulo : nat -> nat -> nat.
+     Definition gcd : nat -> nat -> nat.
+     Definition sqrt_iter : nat -> nat -> nat -> nat -> nat.
+     Definition sqrt : nat -> nat.
+     Definition log2_iter : nat -> nat -> nat -> nat -> nat.
+     Definition log2 : nat -> nat.
+     Definition iter : nat -> forall A : Type, (A -> A) -> A -> A.
+     Definition div2 : nat -> nat.
+     Definition testbit : nat -> nat -> bool.
+     Definition shiftl :
+       (fun _ : nat => nat) 0 -> forall n : nat, (fun _ : nat => nat) n.
+     Definition shiftr :
+       (fun _ : nat => nat) 0 -> forall n : nat, (fun _ : nat => nat) n.
+     Definition bitwise : (bool -> bool -> bool) -> nat -> nat -> nat -> nat.
+     Definition land : nat -> nat -> nat.
+     Definition lor : nat -> nat -> nat.
+     Definition ldiff : nat -> nat -> nat.
+     Definition lxor : nat -> nat -> nat.
+     *)
 End NatTimeExamples.
